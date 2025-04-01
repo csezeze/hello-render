e271e975bb0100272b0859a3f5553a17aabac1a7
csezeze <114854108+csezeze@users.noreply.github.com>
2025-04-01 23:47:54 +0300

    User-agent loglaması eklendi

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Loglama middleware'i
 app.use((req, res, next) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const now = new Date().toLocaleString();
-  console.log(`[${now}] Ziyaretçi IP: ${ip}`);
+  const userAgent = req.headers['user-agent'];
+
+  console.log(`[${now}] IP: ${ip} | Agent: ${userAgent} → ${req.method} ${req.url}`);
   next();
 });
 
