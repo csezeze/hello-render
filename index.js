3b7322bd8750d87e2fd7436cd7c710e25d986ef0
csezeze <114854108+csezeze@users.noreply.github.com>
2025-04-01 23:37:14 +0300

    IP ve zaman loglama eklendi

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Loglama middleware'i
+app.use((req, res, next) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const now = new Date().toLocaleString();
+  console.log(`[${now}] Ziyaretçi IP: ${ip}`);
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Merhaba Zeynep! İlk sunucun çalışıyor 🎉');
 });
