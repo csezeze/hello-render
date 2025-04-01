53df6fce27cf8611f7baf0ca2d8d2d65069659ea
csezeze <114854108+csezeze@users.noreply.github.com>
2025-04-02 00:24:33 +0300

    Ziyaretçi sayacı düzeltildi

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-let ziyaretciSayaci=0;
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  const now = new Date().toLocaleString();
-  const userAgent = req.headers['user-agent'];
-
-  console.log(`[${now}] IP: ${ip} | Agent: ${userAgent} → ${req.method} ${req.url}`);
-  next();
-});
+let ziyaretSayaci = 0;
 
 app.get('/', (req, res) => {
-  res.send('Merhaba Zeynep! İlk sunucun çalışıyor 🦥');
   ziyaretSayaci++;
-  res.send ('Zeynep sana bir haberim var ! Bu sayfa ${ziyaretSayaci} kez ziyaret edildi.`);
+  res.send(`Merhaba Zeynep! İlk sunucun çalışıyor 🎉🦥<br>Bu sayfa ${ziyaretSayaci} kez ziyaret edildi.`);
 });
 
 app.listen(port, () => {
